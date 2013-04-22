31609ed4ee652cd312f43ca3ab61215f0ae4b499
Sergey Vetyutnev <serg.vetyutnev@gmail.com>
2013-04-22 23:00:38 +0200

    Fixing pom's structure for persistence module

--- core/slee/services/txsmppserversbb/src/main/java/org/mobicents/smsc/slee/services/smpp/server/tx/TxSmppServerSbb.java
+++ core/slee/services/txsmppserversbb/src/main/java/org/mobicents/smsc/slee/services/smpp/server/tx/TxSmppServerSbb.java
@@ -23,9 +23,7 @@
 package org.mobicents.smsc.slee.services.smpp.server.tx;
 
 import java.sql.Timestamp;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
---
+++ core/slee/services/txsmppserversbb/src/test/java/org/mobicents/smsc/slee/services/smpp/server/tx/TxSmppServerSbbTest.java
@@ -0,0 +1,17 @@
+package org.mobicents.smsc.slee.services.smpp.server.tx;
+
+import org.mobicents.slee.ChildRelationExt;
+import org.mobicents.smsc.slee.services.smpp.server.tx.TxSmppServerSbb;
+
+public class TxSmppServerSbbTest {
+
+	private class TxSmppServerSbbProxy extends TxSmppServerSbb {
+
+		@Override
+		public ChildRelationExt getStoreSbb() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+}
