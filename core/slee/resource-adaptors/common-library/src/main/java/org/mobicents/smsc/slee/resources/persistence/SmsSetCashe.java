8fe68774874bb07dabe6685ad68f09262e541755
Sergey Vetyutnev <serg.vetyutnev@gmail.com>
2013-05-17 19:35:56 +0200

    Fixing bugs in deployment

--- core/slee/resource-adaptors/common-library/src/main/java/org/mobicents/smsc/slee/resources/persistence/SmsSetCashe.java
+++ core/slee/resource-adaptors/common-library/src/main/java/org/mobicents/smsc/slee/resources/persistence/SmsSetCashe.java
@@ -89,7 +89,7 @@ public class SmsSetCashe {
 	}
 
 	public class TargetAddressContainer {
-		private TargetAddress targetAddress;
-		private int count;
+		public TargetAddress targetAddress;
+		public int count;
 	}
 }
