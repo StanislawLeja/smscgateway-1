779de59ae0fc73dd46b86b6be57580ea460bda88
Stanislaw Leja <stanislaw.leja@ovoo.pl>
2017-11-07 16:23:26 +0100

    Last part of splitted message has partial_esme value instead success_esme. FIXED

--- core/slee/services/sbbs/src/main/java/org/mobicents/smsc/slee/services/deliverysbb/PendingRequestsList.java
+++ core/slee/services/sbbs/src/main/java/org/mobicents/smsc/slee/services/deliverysbb/PendingRequestsList.java
@@ -68,7 +68,7 @@ public class PendingRequestsList implements Serializable {
                 //res.msgPartNumber = 1;
                 if(this.udhData[i1] != null){
                     if(this.udhData[i1].length > 1){
-                        res.udhData = this.udhData[i1][udhData[i1].length-1];
+                        res.udhData = this.udhData[i1][0];
                     }
                 }
                 if (this.sequenceNumbersExtra[i1] != null)
@@ -87,7 +87,7 @@ public class PendingRequestsList implements Serializable {
                         res.sequenceNumberFound = true;
                         res.msgNum = i1;
                         res.splittedMessage = true;
-                        res.udhData = this.udhData[i1][i2];
+                        res.udhData = this.udhData[i1][i2+1];
                         //res.msgPartNumber = i2+2;
                         if (isSent(i1)) {
                             res.confirmed = true;
